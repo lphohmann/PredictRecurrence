ad9d3aae665a9d5a34ecafe8b63c5f6abdc9e681
Lennart Hohmann <l.p.hohmann@gmail.com>
2025-04-24 16:47:24 +0200

    global test methlyl data and start predictiveness script

---
+++ scripts/SCANB_EDA/SCANB_TNBC_GlobalTestMethyl.r
@@ -0,0 +1,88 @@
+#!/usr/bin/env Rscript
+# Script: Project cohort - Global test on methylation data in TNBC
+# Author: Lennart Hohmann
+# Date: 24.04.2025
+#-------------------
+# empty environment
+rm(list=ls())
+# set working directory to the project directory
+setwd("~/PhD_Workspace/PredictRecurrence/")
+#-------------------
+# packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(data.table, globaltest)
+
+#-------------------
+# set/create output directories
+output_path <- "./output/SCANB_GlobalTest/"
+dir.create(output_path, showWarnings = FALSE)
+#-------------------
+# input paths
+infile_1 <- "./data/raw/tnbc_anno.csv"
+infile_2 <- "./data/raw/tnbc235.csv"
+#-------------------
+# output paths
+outfile_1 <- paste0(output_path, "SCANB_TNBC_GlobalTestMethyl.txt")
+#-------------------
+# storing objects 
+results <- list() # object to store results
+
+#######################################################################
+# load data
+#######################################################################
+
+clinical <- read.csv(infile_1)
+dat <- data.table::fread(infile_2)
+dat <- as.data.frame(dat)
+
+# Move gene names to rownames
+rownames(dat) <- dat$V1
+dat <- dat[, -1]
+#dat[1:5,1:5]
+
+# Ensure clinical data has the same samples as dat
+clinical <- clinical[clinical$PD_ID %in% colnames(dat), ]
+
+# Ensure the samples in clinical data are in the same order as in dat
+clinical <- clinical[match(colnames(dat), clinical$PD_ID), ]
+
+head(clinical)
+#identical(clinical$PD_ID,colnames(dat))
+
+#######################################################################
+# Run Global Test
+#######################################################################
+
+# Define outcomes
+outcomes <- c("RFIbin", "OSbin")
+#outcome <- "RFIbin"
+for (outcome in outcomes) {
+
+    # select only samples with available outcome measure data
+    sub_clinical <- clinical[!is.na(clinical[[outcome]]), ]
+    sub_dat <- dat[, colnames(dat) %in% sub_clinical$PD_ID]
+    #identical(colnames(sub_dat), sub_clinical$PD_ID) # TRUE
+
+    # Ensure the outcome is numeric
+    sub_clinical[[outcome]] <- as.numeric(as.character(sub_clinical[[outcome]]))
+    
+    # Run the global test
+    gt_result <- gt(sub_clinical[[outcome]], t(sub_dat)) # Transpose dat to match the expected format
+    
+    
+    # Store the result
+    results[[outcome]] <- summary(gt_result)
+}
+
+#######################################################################
+# Save results to file
+#######################################################################
+
+sink(outfile_1)
+for (outcome in outcomes) {
+  cat("Global Test Results for", outcome, "\n")
+  print(results[[outcome]])
+  cat("\n\n")
+}
+sink()
+
--- src/untils.R
+++ src/untils.R
@@ -16,8 +16,56 @@ m2beta <- function(m) {
   return(beta)
 }
 
+## risk: an object class of "get_risk"  
+## marker the continuous biomarquer 
+## status the event for a binary outcome or status for a time-to-event oucome.
+plotpredictiveness <- function(risk,marker,status){
+  xlim <- c(0,100)
+  breaks = seq(xlim[1], xlim[2], length.out = 5)
+  k <- length(risk)
+  x1<- sort(marker)
+  y1 <- (((1:k)*100)/k)
+  z <- sort(risk)
+  prev<-sum(status)/length(status)
+  Qt<-y1[which.min(abs(z-prev))]
+  Tr<-quantile(x1, Qt/100)
+  plot(y1,z,type='l',xlab="(%)population \n marker Value",ylab="Risk given marker (%)",main="Estimating the predictiveness curve of a continuous biomarker",
+       ylim=c(0,1),axes=FALSE)
+  axis(1,at= c(seq(from=0,to=100,by=20),Qt), label = c(seq(from=0,to=100,by=20),round(Qt, 2)))
+  axis(2,at=seq(from=0,to=1,by=0.2),labels=seq(from=0,to=1,by=0.2)*100,las=2)
+  axis(1,at= c(breaks,Qt), label = round(c(quantile(x1, prob = breaks/100),Tr), 2),pos=-0.26)
+  abline(h=prev,col="blue",lty=2)
+  abline(v=Qt,col="blue",lty=2)
+  return(Threshold=Tr)
+}
+
+## a ’formula’ object consisting of outcome ~ markers and covariables 
+## The outcome can be binary or a ’Surv’ object for time-to-event outcomes
+## data.frame object used to fit and evaluate the model.
+## prediction time used only when the outcome is a time-to-event Surv object
+get_risk <- function(formula,data,prediction.time=NULL,family){
+  event.name <- as.character(formula[[2]])
+  if(event.name[[1]] == "Surv"){
+    cox <- do.call(coxph,list(formula,data))
+    sfit.a0 <- survfit(cox,newdata=data,se.fit = FALSE, conf.type = "none")
+    if(prediction.time==0){
+    return( c(1-summary(sfit.a0)$surv))
+    }
+    return( c(1-summary(sfit.a0,times=prediction.time)$surv))
+  }
+  else{
+    yy <- data[[event.name]]
+    if(  length(unique(yy))==2){
+          outcome = "binary"
+        family = binomial(link ="logit")
+      #event = data[[as.character(formula[[2]])]]
+      myglm<- glm(formula, data = data, family=family)
+    return(myglm$fitted.value)
+   
+    }    
+  }  
+} 
 
 # variance filter for CpGs
-
-
 # Tumor cell content correction
+
